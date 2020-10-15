133ed74a8bd9a42236fad70fa24fa66359a5b8d4
mattyharris1 <matthew.harris@digital.hmrc.gov.uk>
2020-10-15 14:11:27 +0100

    MDS-initial-test-setup | Added some unit tests

---
+++ test/unit/uk/gov/hmrc/individualsifapistub/util/ApiPlatformTestUserConnectorSpec.scala
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.uk.gov.hmrc.individualsifapistub.util
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock._
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import org.scalatest.BeforeAndAfterEach
+import play.api.Configuration
+import play.api.http.Status.{BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND, OK}
+import uk.gov.hmrc.domain.{EmpRef, Nino, SaUtr}
+import uk.gov.hmrc.http.{BadRequestException, HeaderCarrier, HttpClient}
+import uk.gov.hmrc.individualsifapistub.connector.ApiPlatformTestUserConnector
+import uk.gov.hmrc.individualsifapistub.domain._
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+
+class ApiPlatformTestUserConnectorSpec
+    extends TestSupport
+    with BeforeAndAfterEach {
+
+  val stubPort = sys.env.getOrElse("WIREMOCK", "11121").toInt
+  val stubHost = "localhost"
+  val wireMockServer = new WireMockServer(wireMockConfig().port(stubPort))
+  val empRef = EmpRef("123", "AI45678")
+  val testOrganisation = TestOrganisation(
+    Some(empRef),
+    TestOrganisationDetails(
+      "Disney Inc",
+      TestAddress("Capital Tower", "Aberdeen", "SW1 4DQ")))
+
+  val nino = Nino("AB123456A")
+  val utr = SaUtr("2432552635")
+
+  val http: HttpClient = fakeApplication.injector.instanceOf[HttpClient]
+  val config: Configuration = fakeApplication.injector.instanceOf[Configuration]
+  val serviceConfig: ServicesConfig =
+    fakeApplication.injector.instanceOf[ServicesConfig]
+
+  trait Setup {
+    implicit val hc = HeaderCarrier()
+
+    val underTest =
+      new ApiPlatformTestUserConnector(config, http, serviceConfig) {
+        override val serviceUrl = s"http://$stubHost:$stubPort"
+      }
+  }
+
+  override def beforeEach() {
+    wireMockServer.start()
+    configureFor(stubHost, stubPort)
+  }
+
+  override def afterEach() {
+    wireMockServer.stop()
+  }
+
+  "get organisation by empRef" should {
+
+    "retrieve a test organisation by empRef" in new Setup {
+      stubFor(
+        get(urlEqualTo(s"/organisations/empref/${empRef.encodedValue}"))
+          .willReturn(
+            aResponse()
+              .withStatus(OK)
+              .withBody(
+                s"""
+               {
+                 "empRef": "${empRef.value}",
+                 "organisationDetails": {
+                   "name": "Disney Inc",
+                   "address": {
+                     "line1": "Capital Tower",
+                     "line2": "Aberdeen",
+                     "postcode": "SW1 4DQ"
+                   }
+                 }
+               }
+             """
+              )))
+
+      val result = await(underTest.getOrganisationByEmpRef(empRef))
+
+      result shouldBe Some(testOrganisation)
+    }
+
+    "return nothing if the organisation cannot be found" in new Setup {
+      stubFor(
+        get(urlEqualTo(s"/organisations/empref/${empRef.encodedValue}"))
+          .willReturn(aResponse().withStatus(NOT_FOUND)))
+
+      await(underTest.getOrganisationByEmpRef(empRef)) shouldBe None
+    }
+
+    "propagate errors" in new Setup {
+      stubFor(
+        get(urlEqualTo(s"/organisations/empref/${empRef.encodedValue}"))
+          .willReturn(aResponse().withStatus(BAD_REQUEST)))
+
+      intercept[BadRequestException](
+        await(underTest.getOrganisationByEmpRef(empRef)))
+    }
+  }
+
+  "getIndividualByNino" should {
+
+    "retrieve the individual" in new Setup {
+      stubFor(
+        get(urlEqualTo(s"/individuals/nino/$nino"))
+          .willReturn(
+            aResponse()
+              .withStatus(OK)
+              .withBody(s"""{"saUtr": "${utr.value}"}""")))
+
+      val result = await(underTest.getIndividualByNino(nino))
+
+      result shouldBe TestIndividual(Some(utr))
+    }
+
+    "fail with RecordNotFoundException when there is no individual matching the nino" in new Setup {
+      stubFor(
+        get(urlEqualTo(s"/individuals/nino/$nino"))
+          .willReturn(aResponse().withStatus(NOT_FOUND)))
+
+      intercept[RecordNotFoundException] {
+        await(underTest.getIndividualByNino(nino))
+      }
+    }
+
+    "propagate errors" in new Setup {
+      stubFor(
+        get(urlEqualTo(s"/individuals/nino/$nino"))
+          .willReturn(aResponse().withStatus(INTERNAL_SERVER_ERROR)))
+
+      intercept[BadRequestException](
+        await(underTest.getOrganisationByEmpRef(empRef)))
+    }
+
+  }
+}
---
+++ test/unit/uk/gov/hmrc/individualsifapistub/util/IntervalQueryStringBinderSpec.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.uk.gov.hmrc.individualsifapistub.util
+
+import org.joda.time.LocalDateTime.parse
+import org.joda.time.{Interval, LocalDateTime}
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{EitherValues, FlatSpec, Matchers}
+import uk.gov.hmrc.individualsifapistub.util.{Dates, IntervalQueryStringBinder}
+
+class IntervalQueryStringBinderSpec extends FlatSpec with Matchers with EitherValues {
+
+  private val intervalQueryStringBinder = new IntervalQueryStringBinder
+
+  "Interval query string binder" should "fail to bind a missing or malformed from or a malformed to parameter" in new TableDrivenPropertyChecks {
+    val fixtures = Table(
+      ("parameters", "response"),
+      (Map[String, Seq[String]]().empty, """{"code":"INVALID_REQUEST","message":"from is required"}"""),
+      (Map("from" -> Seq.empty[String]), """{"code":"INVALID_REQUEST","message":"from is required"}"""),
+      (Map("from" -> Seq("")), """{"code":"INVALID_REQUEST","message":"from: invalid date format"}"""),
+      (Map("from" -> Seq("20200131")), """{"code":"INVALID_REQUEST","message":"from: invalid date format"}"""),
+      (Map("from" -> Seq("2020-01-31"), "to" -> Seq("")), """{"code":"INVALID_REQUEST","message":"to: invalid date format"}"""),
+      (Map("from" -> Seq("2020-01-31"), "to" -> Seq("20201231")), """{"code":"INVALID_REQUEST","message":"to: invalid date format"}""")
+    )
+
+    fixtures foreach { case (parameters, response) =>
+      val maybeEither = intervalQueryStringBinder.bind("", parameters)
+      maybeEither.isDefined shouldBe true
+      maybeEither.get.isLeft shouldBe true
+      maybeEither.get.left.value shouldBe response
+    }
+  }
+
+  it should "default to today's date when a valid from parameter is present but a to parameter is missing" in {
+    val parameters = Map("from" -> Seq("2017-01-31"))
+    val maybeEither = intervalQueryStringBinder.bind("", parameters)
+    maybeEither.isDefined shouldBe true
+    maybeEither.get.isRight shouldBe true
+    maybeEither.get.right.value shouldBe toInterval("2017-01-31T00:00:00.000", LocalDateTime.now().withTime(0, 0, 0, 1).toString())
+  }
+
+  it should "succeed in binding an interval from well formed from and to parameters" in {
+    val parameters = Map("from" -> Seq("2020-01-31"), "to" -> Seq("2020-12-31"))
+    val maybeEither = intervalQueryStringBinder.bind("", parameters)
+    maybeEither.isDefined shouldBe true
+    maybeEither.get.isRight shouldBe true
+    maybeEither.get.right.value shouldBe toInterval("2020-01-31T00:00:00.000", "2020-12-31T00:00:00.001")
+  }
+
+  it should "fail to bind an interval from an invalid date range" in {
+    val parameters = Map("from" -> Seq("2020-12-31"), "to" -> Seq("2020-01-31"))
+    val maybeEither = intervalQueryStringBinder.bind("", parameters)
+    maybeEither.isDefined shouldBe true
+    maybeEither.get.isLeft shouldBe true
+    maybeEither.get.left.value shouldBe """{"code":"INVALID_REQUEST","message":"Invalid time period requested"}"""
+  }
+
+  it should "unbind intervals to query parameters" in {
+    val interval = toInterval("2020-01-31", "2020-12-31")
+    intervalQueryStringBinder.unbind("", interval) shouldBe "from=2020-01-31&to=2020-12-31"
+  }
+
+  private def toInterval(from: String, to: String): Interval =
+    Dates.toInterval(parse(from).toLocalDate, parse(to).toLocalDate)
+
+}
