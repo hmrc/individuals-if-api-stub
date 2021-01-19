f4114262b297d0b4ca414e770b5a2b05332bd11e
ajchilts <40767309+ajchilts@users.noreply.github.com>
2021-01-19 11:41:58 +0000

    Update licence comment

--- test/unit/uk/gov/hmrc/individualsifapistub/util/controllers/DetailsControllerSpec.scala
+++ test/unit/uk/gov/hmrc/individualsifapistub/util/controllers/DetailsControllerSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,6 +40,9 @@ class DetailsControllerSpec extends TestSupport with TestHelpers {
 
   val idType = "nino"
   val idValue = "XH123456A"
+  val startDate = "2020-01-01"
+  val endDate = "2020-21-31"
+  val consumer = "TEST"
 
   val request = CreateDetailsRequest(
     Some(Seq(ContactDetail(9, "MOBILE TELEPHONE", "07123 987654"), ContactDetail(9,"MOBILE TELEPHONE", "07123 987655"))),
@@ -50,11 +53,16 @@ class DetailsControllerSpec extends TestSupport with TestHelpers {
 
   "Create details" should {
     "Successfully create a details record and return created record as response" in new Setup {
-      val details = Identifier(Some(idValue), None)
-      val detailsResponse = DetailsResponse(details, request.contactDetails, request.residences)
+      val ident = Identifier(Some(idValue), None, startDate, endDate, Some(consumer))
+      val id = s"${ident.nino.getOrElse(ident.trn)}-$startDate-$endDate-$consumer"
+      val detailsResponse = DetailsResponse(id, request.contactDetails, request.residences)
 
-      when(mockDetailsService.create(idType, idValue, request)).thenReturn(Future.successful(detailsResponse))
-      val result = await(underTest.create(idType, idValue)(fakeRequest.withBody(Json.toJson(request))))
+      when(mockDetailsService.create(
+        idType, idValue, startDate, endDate, consumer, request)
+      ).thenReturn(Future.successful(detailsResponse))
+
+      val result = await(underTest.create(idType, idValue, startDate, endDate, consumer)
+      (fakeRequest.withBody(Json.toJson(request))))
 
       status(result) shouldBe CREATED
       jsonBodyOf(result) shouldBe Json.toJson(detailsResponse)
