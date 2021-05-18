f8a6302015757c32547d82fb07688f3c6875d7be
parkeradam <30392071+parkeradam@users.noreply.github.com>
2021-05-18 16:58:05 +0100

    HODS-127 : Added Number of employees request and tests surrounding json.

---
+++ app/uk/gov/hmrc/individualsifapistub/domain/organisations/NumberOfEmployees.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.individualsifapistub.domain.organisations
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Reads._
+import play.api.libs.json.{Format, JsPath}
+
+case class NumberOfEmployeeCounts(dateTaken: String, employeeCount: Int)
+case class NumberOfEmployeeReferences(districtNumber: String, payeReference: String, counts: Seq[NumberOfEmployeeCounts])
+case class CreateNumberOfEmployeesRequest(startDate: String, endDate: String, references: Seq[NumberOfEmployeeReferences])
+
+object NumberOfEmployees {
+
+  val dateTakenPattern = "^[1-2]{1}[0-9]{3}-[0-9]{2}$".r
+  val districtNumberPattern = "^[0-9]{3}$".r
+  val payeRefPattern = "^[a-zA-Z0-9]{1,10}$".r
+  val datePattern = "^(((19|20)([2468][048]|[13579][26]|0[48])|2000)[-]02[-]29|((19|20)[0-9]{2}[-](0[469]|11)[-](0[1-9]|1[0-9]|2[0-9]|30)|(19|20)[0-9]{2}[-](0[13578]|1[02])[-](0[1-9]|[12][0-9]|3[01])|(19|20)[0-9]{2}[-]02[-](0[1-9]|1[0-9]|2[0-8])))$".r
+
+  implicit val numberOfEmployeeCountsFormat = Format[NumberOfEmployeeCounts](
+    (
+      (JsPath \ "dateTaken").read[String](pattern(dateTakenPattern, "Invalid dateTaken format")) and
+        (JsPath \ "employeeCount").read[Int]
+      )(NumberOfEmployeeCounts.apply _),
+    (
+      (JsPath \ "dateTaken").write[String] and
+        (JsPath \ "employeeCount").write[Int]
+      )(unlift(NumberOfEmployeeCounts.unapply))
+  )
+
+  implicit val numberOfEmployeeReferencesFormat = Format(
+    (
+      (JsPath \ "districtNumber").read[String](pattern(districtNumberPattern, "District number is invalid")) and
+        (JsPath \ "payeReference").read[String](pattern(payeRefPattern, "payeReference is invalid")) and
+        (JsPath \ "counts").read[Seq[NumberOfEmployeeCounts]]
+      )(NumberOfEmployeeReferences.apply _),
+    (
+      (JsPath \ "districtNumber").write[String] and
+        (JsPath \ "payeReference").write[String] and
+        (JsPath \ "counts").write[Seq[NumberOfEmployeeCounts]]
+      )(unlift(NumberOfEmployeeReferences.unapply))
+  )
+
+  implicit val createNumberOfEmployeesRequestFormat = Format(
+    (
+      (JsPath \ "startDate").read[String](pattern(datePattern, "startDate is invalid")) and
+        (JsPath \ "endDate").read[String](pattern(datePattern, "endDate is invalid")) and
+        (JsPath \ "references").read[Seq[NumberOfEmployeeReferences]]
+      )(CreateNumberOfEmployeesRequest.apply _),
+    (
+      (JsPath \ "startDate").write[String] and
+        (JsPath \ "endDate").write[String] and
+        (JsPath \ "references").write[Seq[NumberOfEmployeeReferences]]
+      )(unlift(CreateNumberOfEmployeesRequest.unapply))
+  )
+}
---
+++ test/unit/uk/gov/hmrc/individualsifapistub/util/domain/organisations/NumberOfEmployeesSpec.scala
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.uk.gov.hmrc.individualsifapistub.util.domain.organisations
+
+import play.api.libs.json.Json
+import uk.gov.hmrc.individualsifapistub.domain.organisations.{CreateNumberOfEmployeesRequest, NumberOfEmployeeCounts, NumberOfEmployeeReferences}
+import uk.gov.hmrc.individualsifapistub.domain.organisations.NumberOfEmployees._
+import unit.uk.gov.hmrc.individualsifapistub.util.UnitSpec
+
+class NumberOfEmployeesSpec extends UnitSpec {
+  "NumberOfEmployeeCounts reads from JSON successfully" in {
+    val json =
+      """
+        |{
+        |    "dateTaken": "2019-10",
+        |    "employeeCount": 554
+        |}
+        |""".stripMargin
+
+    val expectedResult = NumberOfEmployeeCounts("2019-10", 554)
+
+    val result = Json.parse(json).validate[NumberOfEmployeeCounts]
+
+    result.isSuccess shouldBe true
+    result.get shouldBe expectedResult
+  }
+
+  "NumberOfEmployeeCounts reads from JSON unsuccessfully when dateTake is invalid" in {
+    val json =
+      """
+        |{
+        |    "dateTaken": "20191-10",
+        |    "employeeCount": 554
+        |}
+        |""".stripMargin
+
+    val result = Json.parse(json).validate[NumberOfEmployeeCounts]
+
+    result.isSuccess shouldBe false
+  }
+
+  "NumberOfEmployeeReferences reads from JSON Successfully" in {
+    val json =
+      """
+        |{
+        |  "districtNumber": "456",
+        |  "payeReference": "RT882d",
+        |  "counts": []
+        |}
+        |""".stripMargin
+
+    val expectedresult = NumberOfEmployeeReferences("456", "RT882d", Seq.empty)
+
+    val result = Json.parse(json).validate[NumberOfEmployeeReferences]
+
+    result.isSuccess shouldBe true
+    result.get shouldBe expectedresult
+  }
+
+  "NumberOfEmployeeReferences reads from JSON unsuccessfully when districtNumber is incorrect" in {
+    val json =
+      """
+        |{
+        |  "districtNumber": "ABC",
+        |  "payeReference": "RT882d",
+        |  "counts": []
+        |}
+        |""".stripMargin
+
+    val result = Json.parse(json).validate[NumberOfEmployeeReferences]
+
+    result.isSuccess shouldBe false
+  }
+
+  "NumberOfEmployeeReferences reads from JSON unsuccessfully when payeReference is incorrect" in {
+    val json =
+      """
+        |{
+        |  "districtNumber": "456",
+        |  "payeReference": "12345678901234",
+        |  "counts": []
+        |}
+        |""".stripMargin
+
+    val result = Json.parse(json).validate[NumberOfEmployeeReferences]
+
+    result.isSuccess shouldBe false
+  }
+
+  "CreateNumberOfEmployeesRequest reads from JSON successfully" in {
+    val json =
+      """
+        |{
+        |  "startDate": "2019-10-01",
+        |  "endDate": "2020-04-05",
+        |  "references": []
+        |}
+        |""".stripMargin
+
+    val expectedResult = CreateNumberOfEmployeesRequest("2019-10-01", "2020-04-05", Seq.empty)
+
+    val result = Json.parse(json).validate[CreateNumberOfEmployeesRequest]
+
+    result.isSuccess shouldBe true
+    result.get shouldBe expectedResult
+  }
+
+  "CreateNumberOfEmployeesRequest reads from JSON unsuccessfully when startDate is incorrect" in {
+    val json =
+      """
+        |{
+        |  "startDate": "20111-10-01",
+        |  "endDate": "2020-04-05",
+        |  "references": []
+        |}
+        |""".stripMargin
+
+    val result = Json.parse(json).validate[CreateNumberOfEmployeesRequest]
+
+    result.isSuccess shouldBe false
+  }
+
+  "CreateNumberOfEmployeesRequest reads from JSON unsuccessfully when endDate is incorrect" in {
+    val json =
+      """
+        |{
+        |  "startDate": "2019-10-01",
+        |  "endDate": "20222-04-05",
+        |  "references": []
+        |}
+        |""".stripMargin
+
+    val result = Json.parse(json).validate[CreateNumberOfEmployeesRequest]
+
+    result.isSuccess shouldBe false
+  }
+}
