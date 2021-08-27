4530d4537ff483f049258d09f9538ad8c0717ce5
parkeradam <30392071+parkeradam@users.noreply.github.com>
2021-08-27 10:59:34 +0100

    HODS-382 : Updating bootstrap-backend-play for auth update.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,11 +6,11 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "5.7.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.12.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "5.7.0"                 % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.12.0"                 % Test,
     "org.scalatest"           %% "scalatest"                % "3.2.9"                 % Test,
     "com.typesafe.play"       %% "play-test"                % current                 % Test,
     "com.vladsch.flexmark"     %  "flexmark-all"              % "0.35.10"             % "test, it",
