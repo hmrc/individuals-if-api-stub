d710175e4b2b1f5001d34a294a356bbe4397c7de
johno1985 <1640705+johno1985@users.noreply.github.com>
2024-10-02 15:56:22 +0100

    DLS-10898: Removed unused dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,11 +14,9 @@
  * limitations under the License.
  */
 
-import play.sbt.PlayImport.*
 import sbt.*
 
 object AppDependencies {
-
   val hmrc             = "uk.gov.hmrc"
   val hmrcMongo        = "uk.gov.hmrc.mongo"
   val hmrcMongoVersion = "1.7.0"
@@ -26,7 +24,6 @@ object AppDependencies {
   val playVersion      = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
-    ws,
     hmrc      %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
     hmrc      %% s"domain-$playVersion"            % "9.0.0",
     hmrc      %% s"play-hal-$playVersion"          % "4.0.0",
@@ -34,11 +31,8 @@ object AppDependencies {
   )
 
   def test(scope: Configuration = Test): Seq[ModuleID] = Seq(
-    "org.scalatestplus.play" %% "scalatestplus-play"             % "5.1.0"          % scope,
     "org.scalatestplus"      %% "mockito-3-4"                    % "3.2.10.0"       % scope,
-    "com.vladsch.flexmark"   % "flexmark-all"                    % "0.64.8"         % scope,
     "org.scalaj"             %% "scalaj-http"                    % "2.4.2"          % scope,
-    "org.pegdown"            % "pegdown"                         % "1.6.0"          % scope,
     hmrcMongo                %% s"hmrc-mongo-test-$playVersion"  % hmrcMongoVersion % scope,
     hmrc                     %% s"bootstrap-test-$playVersion"   % bootstrapVersion % scope
   )
