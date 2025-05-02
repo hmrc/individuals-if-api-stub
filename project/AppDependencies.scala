7ab6d7dc1365100ac80b10f81484cb80d3facbb9
johno1985 <1640705+johno1985@users.noreply.github.com>
2025-05-02 17:24:40 +0100

    DLS-11627: Removed unused dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -26,12 +26,10 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     hmrc      %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
     hmrc      %% s"domain-$playVersion"            % "9.0.0",
-    hmrc      %% s"play-hal-$playVersion"          % "4.0.0",
     hmrcMongo %% s"hmrc-mongo-$playVersion"        % hmrcMongoVersion
   )
 
   def test(scope: Configuration = Test): Seq[ModuleID] = Seq(
-    "org.scalaj"             %% "scalaj-http"                    % "2.4.2"          % scope,
     hmrcMongo                %% s"hmrc-mongo-test-$playVersion"  % hmrcMongoVersion % scope,
     hmrc                     %% s"bootstrap-test-$playVersion"   % bootstrapVersion % scope
   )
